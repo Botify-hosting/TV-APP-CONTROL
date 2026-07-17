24f0fb928f14101aa8d9c0dd4cf9ef38e2edd34c
Botify <levi.jvbcollege@gmail.com>
2026-07-17 14:37:13 +0200

    Update config.js

--- config.js
+++ config.js
@@ -7,25 +7,12 @@
 module.exports = {
 
 
-    tv: {
-
-
-        // Vul hier straks het IP-adres van je Philips TV in
-
-        ip:
-        "192.168.1.XXX",
-
-
-        port:
-        1926,
-
-
-        name:
-        "Philips OLED855"
-
-
-    },
-
+ tv:{
+    ip:"192.168.2.12",
+    port:1926,
+    name:"Philips OLED855/12",
+    mac:"68:07:0a:50:ed:0e"
+}
 
 
     server:{
