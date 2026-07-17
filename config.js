e8a2cd8a2d3b982703a531f5966e29e53bde35f5
Botify <levi.jvbcollege@gmail.com>
2026-07-17 14:33:01 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,41 @@
+/*
+    TVControl Pro
+    Configuratie
+*/
+
+
+module.exports = {
+
+
+    tv: {
+
+
+        // Vul hier straks het IP-adres van je Philips TV in
+
+        ip:
+        "192.168.1.XXX",
+
+
+        port:
+        1926,
+
+
+        name:
+        "Philips OLED855"
+
+
+    },
+
+
+
+    server:{
+
+
+        port:
+        3000
+
+
+    }
+
+
+};
